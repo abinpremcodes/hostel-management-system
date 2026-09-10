e5db51c4d8103cff07d863b5755e185aeaf0824e
ABIN PREM.T <abinprem2590@gmail.com>
2026-09-10 11:52:45 +0530

    Register custom User model in Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display=('username','email','role','is_staff')
+    fieldsets=UserAdmin.fieldsets + (
+    ('Role Info',{'fields': ('role', 'phone')}),
+    )
+    
+
