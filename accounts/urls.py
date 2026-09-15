b56edfded980e55d51c5967954d638fa8f4f73dd
ABIN PREM.T <abinprem2590@gmail.com>
2026-09-15 11:40:21 +0530

    Add student list view and template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from .import views
+
 
 app_name='accounts'
 
@@ -7,4 +9,6 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='accounts:login'), name='logout'),
+    path('students/',views.student_list,name='student_list'),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import StudentProfile
 
-# Create your views here.
+@login_required
+
+def student_list(request):
+    students=StudentProfile.objects.all()
+    return render(request,'accounts/student_list.html',{'students':students})
+
+                  
+
+
