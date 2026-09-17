d988fc90e428c03ebedab73bd37ad41a369e8f49
ABIN PREM.T <abinprem2590@gmail.com>
2026-09-17 11:15:20 +0530

    Register Allocation in admin, verify business rules working

--- allocations/admin.py
+++ allocations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Allocation
 
-# Register your models here.
+
+@admin.register(Allocation)
+class AllocationAdmin(admin.ModelAdmin):
+    list_display = ('student', 'bed', 'allocated_date', 'vacated_date', 'status')
