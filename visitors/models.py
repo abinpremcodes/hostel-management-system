c9636e03f83f6419443c6bcca2836e415862878f
ABIN PREM.T <abinprem2590@gmail.com>
2026-09-21 14:30:02 +0530

    Add Visitor model

--- visitors/models.py
+++ visitors/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
+from accounts .models import StudentProfile
 
-# Create your models here.
+class Visitor(models.Model):
+    student=models.ForeignKey(StudentProfile,on_delete=models.CASCADE,related_name='visitors')
+    visitor_name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=15)
+    purpose=models.CharField(max_length=200,blank=True)
+    check_in=models.DateTimeField(default=timezone.now)
+    check_out=models.DateTimeField(null=True,blank=True)
+
+    def __str__(self):
+        return f" {self.visitor_name} visiting {self.student}"
+
+    @property
+    def is_checked_in(self):
+        return self.check_out is None
+
+
+
