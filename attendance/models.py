f76cbb2cc81945d11ca053f311b2847eb0022e01
ABIN PREM.T <abinprem2590@gmail.com>
2026-09-22 11:39:11 +0530

    Add Attendance model

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import StudentProfile
 
-# Create your models here.
+class Attendance(models.Model):
+    class Status(models.TextChoices):
+        PRESENT='PRESENT','Present'
+        ABSENT='ABSENT','Absent'
+
+
+    student=models.ForeignKey(StudentProfile,on_delete=models.CASCADE,related_name='attendance_records')
+    date=models.DateField()
+    status=models.CharField(max_length=15,choices=Status.choices,default=Status.PRESENT)
+
+
+    class Meta:
+        unique_together=('student','date')
+
+    def __str__(self):
+        return f"Attendance {self.student} - {self.date} - {self.get_status_display()}"
+    
+
+
+    
